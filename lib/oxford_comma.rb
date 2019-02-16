fc93d2e81e6430392a945f6eb74ed8e8b0f1eb2a
Tyler Hunt <tyhunt04@gmail.com>
2019-02-16 20:13:07 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array.split
+  elsif array.length == 2
+    return array.split("and")
+  elsif array.length == 3
+    return array.split()
+  else array.length > 3
+    return array.split()
+  end
 end
